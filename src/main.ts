12e259d66d0e40ca2059cfbf1df63eacfd9e077f
David Garcia <Davidgarciama@outlook.com>
2023-02-10 19:36:17 -0600

    Solved

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
-// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+let xo = (str: string): boolean => {
+    let x:number=0;
+    let o:number=0;
+    for (let i = 0; i < str.length; i++) {
+        if(str[i].toLowerCase() == 'x'){
+            x++
+        } else if(str[i].toLowerCase() == 'o') {
+            o++
+        }
+    }
+    return x==o?true:false
+}
 
-// Examples input/output:
-
-// XO("ooxx") => true
-// XO("xooxx") => false
-// XO("ooxXm") => true
-// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
-// XO("zzoo") => false
-
-export function xo(str: string) : boolean {
-    // your code here
-  }
-
-  console.log(xo('xo'))//,true);
-  console.log(xo("xxOo"))//,true);
-  console.log(xo("xxxm"))//,false);
-  console.log(xo("Oo"))//,false);
-  console.log(xo("ooom"))//,false);
+//   console.log(xo('xo'))//,true);
+//   console.log(xo("xxOo"))//,true);
+//   console.log(xo("xxxm"))//,false);
+//   console.log(xo("Oo"))//,false);
+//   console.log(xo("ooom"))//,false);
